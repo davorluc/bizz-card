84324d67596bcae419874d7808f4578cbb52a396
davorluc <d.lucic4@hotmail.com>
2025-02-18 19:44:43 +0100

    explanation of initial eth funding

--- ignition/modules/DomainsModule.js
+++ ignition/modules/DomainsModule.js
@@ -4,7 +4,7 @@ module.exports = buildModule("DomainsModule", (m) => {
   const tld = m.getParameter("tld", "bizz"); // Default TLD is "bizz"
 
   const domains = m.contract("Domains", [tld], {
-    value: m.getParameter("initialFunds", 100000000000000000n), // Optional initial ETH funding
+    value: m.getParameter("initialFunds", 100000000000000000n), // Optional initial ETH funding (0.1)
   });
 
   return { domains };
