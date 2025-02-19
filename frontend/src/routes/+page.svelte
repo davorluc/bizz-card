51427e3536bfcef2976840c3e91c97d0ce4fa133
davorluc <d.lucic4@hotmail.com>
2025-02-19 10:44:42 +0100

    wallet connection and first form fields

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,40 +1,64 @@
 <script>
   import { onMount } from "svelte";
+  import { ethers } from "ethers";
 
   // Constant
   const TWITTER_HANDLE = "puskaimdzep";
   const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-  let walletConnected = false;
+  const tld = ".bizz";
 
-  // Check if the wallet is connected
-  const checkIfWalletIsConnected = () => {
-    // Make sure we have access to window.ethereum
-    const { ethereum } = window;
+  // States
+  let currentAccount = "";
+  let domain = "";
+  let loading = false;
+  let record = "";
 
-    if (!ethereum) {
-      console.log("Make sure you have MetaMask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
-      walletConnected = true;
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+
+      // Fancy method to request access to account.
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      // Boom! This should print out public address once we authorize Metamask.
+      console.log("Connected", accounts[0]);
+      currentAccount = accounts[0];
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  // Call checkIfWalletIsConnected when the component is mounted
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      currentAccount = account;
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
   onMount(() => {
     checkIfWalletIsConnected();
   });
-
-  // Render wallet not connected view
-  const renderNotConnectedContainer = () => {
-    return `
-      <div class="connect-wallet-container">
-        <img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja gif" />
-        <button class="cta-button connect-wallet-button">
-          Connect Wallet
-        </button>
-      </div>
-    `;
-  };
 </script>
 
 <div class="App">
@@ -50,9 +74,51 @@
       </header>
     </div>
 
-    <!-- Add your render method here -->
-    {#if !walletConnected}
-      {@html renderNotConnectedContainer()}
+    {#if !currentAccount}
+      <div class="connect-wallet-container">
+        <img
+          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2hvbnFrMjBmamt4YTNpZHAzbmRlMWVyczZncGpmMjBmOTZvYThkdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Jq1T4jCKm9039q2lEV/giphy.gif"
+          alt="American Psycho Business Card Scene Gif"
+        />
+        <button
+          on:click={connectWallet}
+          class="cta-button connect-wallet-button"
+        >
+          Connect Wallet
+        </button>
+      </div>
+    {/if}
+
+    {#if currentAccount}
+      <div class="form-container">
+        <div class="first-row">
+          <input type="text" placeholder="domain" bind:value={domain} />
+          <p class="tld">{tld}</p>
+        </div>
+
+        <input
+          type="text"
+          placeholder="whats ur ninja power"
+          bind:value={record}
+        />
+
+        <div class="button-container">
+          <button
+            class="cta-button mint-button"
+            disabled={null}
+            on:click={null}
+          >
+            Mint
+          </button>
+          <button
+            class="cta-button mint-button"
+            disabled={null}
+            on:click={null}
+          >
+            Set data
+          </button>
+        </div>
+      </div>
     {/if}
 
     <div class="footer-container">
